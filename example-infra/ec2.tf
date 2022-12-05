8a7e76747864acfee110509d20b410dc220a92e8
= <=>
2022-12-05 23:53:36 +0530

    terraform

--- example-infra/ec2.tf
+++ example-infra/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "this" {
   ami                       = "ami-0b0dcb5067f052a63"
   instance_type             = "t2.micro"
+  security_groups           =  [aws_security_group.allow_tls.id]
 
   tags = {
     Name = "Helloworld"
@@ -13,4 +14,37 @@ output "private_ip" {
 
 output "private_dns" {
     value = aws_instance.this.private_dns 
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  
+
+  ingress {
+    description      = "SSH to security group"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "http to SG"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
