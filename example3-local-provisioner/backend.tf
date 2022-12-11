db2bfe24bcdc0fe85c33d78e4484c654d434de61
= <=>
2022-12-11 07:11:16 +0530

    terrafrom

--- example3-local-provisioner/backend.tf
+++ example3-local-provisioner/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "b51-tf-remote-state-bucket-afs"
-    key    = "sample/terraform.tfstate"
+    key    = "example3/terraform.tfstate"
     region = "us-east-1"
   }
 }
